133d3e6e37bce2fb1b3dfacf2307ac817e842162
Jason Prime <600613.m6@gmail.com>
2023-04-18 11:18:10 +0200

    Update

--- text-blink-effect-with-redirection.js
+++ text-blink-effect-with-redirection.js
@@ -1,7 +1,23 @@
-		// Text blink effect including redirection to an specific URL
-		// Jan Gebser
-		// github@brainhub24.com
+// Text blink effect including redirection to an specific URL
+// https://github.com/Webservice-Digital/404-Error-Landingpage/tree/main
+// Jan Gebser (IT-Consultant)
+// github@brainhub24.com
+// https://webservice.digital
 		
-        setInterval(()=>document.querySelector(".blink").classList.toggle("hide"),600);
-        function redirect(){window.location.replace("https://netcore.digital/");} 
-        window.onkeypress=redirect;
+
+const blinkElement = document.querySelector(".blink");
+if (blinkElement) {
+  setInterval(() => {
+    blinkElement.classList.toggle("hide");
+  }, 600);
+}
+
+// Redirect to the homepage when a key is pressed
+function redirectToHomepage() {
+  // Redirect to NC
+  if (window.location.replace) {
+    window.location.replace("https://netcore.digital/");
+  }
+}
+
+window.onkeypress = redirectToHomepage;
